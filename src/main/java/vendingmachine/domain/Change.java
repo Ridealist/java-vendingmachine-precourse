547f1c0fb251623825853366dfd215f06ea24a81
Ridealist <cjsn92@gmail.com>
2022-12-15 23:26:32 +0900

    feat: 입력값으로 무작위 동전 생성해 자판기가 가진 전체 동전 구하는 기능 구현

--- src/main/java/vendingmachine/domain/Change.java
+++ src/main/java/vendingmachine/domain/Change.java
@@ -1,13 +1,61 @@
 package vendingmachine.domain;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Change {
+    private static final int TOTAL_AMOUNT_LOWER_BOUND = 0;
+    private static final int AMOUNT_NUMBER_UNIT = 10;
 
-    private final Map<Coin, Integer> coins;
+    private static final String INVALID_NUMBER_RANGE_MESSAGE = "금액은 0보다 같거나 큰 10의 배수로 입력해야 합니다.";
 
-    public Map<Coin, Integer> getCoins() {
+    private final Map<Integer, Integer> coins;
+
+    public Change(int number, NumberGenerator numberGenerator) {
+        validateAmount(number);
+        this.coins = initCoins();
+        updateCoins(number, numberGenerator);
+    }
+
+    private Map<Integer, Integer> initCoins() {
+        Map<Integer, Integer> coins = new LinkedHashMap<>();
+        for (int coinAmount : Coin.getAmounts()) {
+            coins.put(coinAmount, 0);
+        }
+        return coins;
+    }
+
+    private void updateCoins(int amount, NumberGenerator numberGenerator) {
+        while (amount > 0) {
+            int number = numberGenerator.generate();
+            if (amount < number) {
+                continue;
+            }
+            coins.put(number, coins.get(number) + 1);
+            amount -= number;
+        }
+    }
+
+    private void validateAmount(int number) {
+        validateNumberRange(number);
+        validateNumberUnit(number);
+    }
+
+    // TODO InputValidator에서 따로 관리 고민해보기!
+    private void validateNumberRange(int number) {
+        if (number < TOTAL_AMOUNT_LOWER_BOUND) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    private void validateNumberUnit(int number) {
+        if (number % AMOUNT_NUMBER_UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE_MESSAGE);
+        }
+    }
+
+    public Map<Integer, Integer> getCoins() {
         return Collections.unmodifiableMap(coins);
     }
 }
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,15 +1,23 @@
 package vendingmachine.view;
 
+import java.util.Map;
+
 public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String STATUS_FORMAT = "%d원 - %d개";
+
 
     public static void printError() {
 
     }
 
-    public static void printCoinStatus() {
-
+    public static void printCoinStatus(Map<Integer, Integer> coins) {
+        System.out.println("자판기가 보유한 동전");
+        for (int amount : coins.keySet()) {
+            System.out.printf(STATUS_FORMAT, amount, coins.get(amount));
+            System.out.println();
+        }
     }
 
     public static void printInputMoneyStatus() {
