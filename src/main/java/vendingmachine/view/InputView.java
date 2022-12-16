dba296ac6ff585a8d3a483f0e789753cb223901b
Ridealist <cjsn92@gmail.com>
2022-12-16 23:39:53 +0900

    feat(InputView): InputParser 및 InputValidator 메소드 기능 연결

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,16 +1,16 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.function.Supplier;
+import java.util.List;
+import vendingmachine.utils.InputParser;
+import vendingmachine.utils.InputValidator;
 
 public class InputView {
     private enum Message {
         INPUT_CHANGE_AMOUNT("자판기가 보유하고 있는 금액을 입력해 주세요."),
         INPUT_PRODUCT_LIST("상품명과 가격, 수량을 입력해 주세요."),
         INPUT_MONEY("투입 금액을 입력해 주세요."),
-        INPUT_PRODUCT_NAME("구매할 상품명을 입력해 주세요."),
-
-        EXCEPTION_NOT_A_NUMBER("숫자를 입력해 주세요.");
+        INPUT_PRODUCT_NAME("구매할 상품명을 입력해 주세요.");
 
         private final String explanation;
 
@@ -22,27 +22,27 @@ public class InputView {
     public static int readChangeAmount() {
         System.out.println(Message.INPUT_CHANGE_AMOUNT.explanation);
         String input = readLineAndBreakOneLine();
-        return validateNumber(input);
+        InputValidator.validateNumber(input);
+        return Integer.parseInt(input);
     }
-
-    public static String readProductList() {
-        return "";
+    
+    public static List<List<String>> readProductList() throws IllegalArgumentException {
+        System.out.println(Message.INPUT_PRODUCT_LIST.explanation);
+        String input = readLineAndBreakOneLine();
+        return InputParser.makeProductsList(input);
     }
 
     public static int readInputMoney() {
-        return 0;
+        System.out.println(Message.INPUT_MONEY.explanation);
+        String input = readLineAndBreakOneLine();
+        InputValidator.validateNumber(input);
+        return Integer.parseInt(input);
     }
 
     public static String readProductName() {
-        return "";
-    }
-
-    public static int validateNumber(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NullPointerException e) {
-            throw new IllegalArgumentException(Message.EXCEPTION_NOT_A_NUMBER.explanation);
-        }
+        System.out.println(Message.INPUT_PRODUCT_NAME.explanation);
+        String input = readLineAndBreakOneLine();
+        return input.trim();
     }
 
     private static String readLineAndBreakOneLine() {
